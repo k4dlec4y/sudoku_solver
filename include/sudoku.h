53dd5dfb8d657a5ede908e94ea035cc4dd307d2c
Matúš Kadlecay <matus.kadlecay@gmail.com>
2025-08-02 21:43:32 +0200

    final cosmetic adjustments for sudoku files

--- include/sudoku.h
+++ include/sudoku.h
@@ -11,63 +11,60 @@
 #include <stdbool.h>
 
 /**
- * @brief Eliminate the possible digits in unknown squares in the row.
+ * @brief  Eliminate the possible digits in unknown squares in the row.
  *
  * E.g. for |...|456|789| the unknown squares will have
  * bitset 7 (000000111).
  *
- * @param sudoku 2D array of digit bitsets
- * @param row_index starting from 0 (leftmost) to 8 (rightmost)
+ * @param sudoku  2D array of digit bitsets
+ * @param row     starting from 0 (leftmost) to 8 (rightmost)
  */
-bool eliminate_row(unsigned int sudoku[9][9], int row_index);
+bool eliminate_row(unsigned int sudoku[9][9], int row);
 
 /**
- * @brief Eliminate the possible digits in unknown squares in the column.
+ * @brief  Eliminate the possible digits in unknown squares in the column.
  *
- * @param sudoku 2D array of digit bitsets
- * @param col_index starting from 0 (top) to 8 (bottom)
+ * @param sudoku  2D array of digit bitsets
+ * @param col     starting from 0 (top) to 8 (bottom)
  */
-bool eliminate_col(unsigned int sudoku[9][9], int col_index);
+bool eliminate_col(unsigned int sudoku[9][9], int col);
 
 /**
- * @brief Eliminate the possible digits in unknown squares in the box.
+ * @brief  Eliminate the possible digits in unknown squares in the box.
  *
- * @param sudoku 2D array of digit bitsets
- * @param row_index of the top most row in box, one of 0, 3, 6
- * @param col_index of the left most column in box, one of 0, 3, 6
+ * @param sudoku  2D array of digit bitsets
+ * @param row     the top most row index of the box, one of 0, 3, 6
+ * @param col     the left most column index of the box, one of 0, 3, 6
  */
-bool eliminate_box(unsigned int sudoku[9][9], int row_index, int col_index);
+bool eliminate_box(unsigned int sudoku[9][9], int row, int col);
 
 /**
- * @brief Check if the sudoku is to be solved yet, or is allready
- * complete.
+ * @brief  Check if the sudoku is to be solved yet, or is already complete.
  *
- * @note Does not care about validity, just checks whether cell
- * without unique value exists!
- * @param sudoku 2D array of digit bitsets
+ * @note  Does not care about validity, just checks whether cell
+ *        without unique value exists!
+ *
+ * @param sudoku  2D array of digit bitsets
  */
 bool needs_solving(unsigned int sudoku[9][9]);
 
 /**
- * @brief Check if the set digits do not conflict with one another.
+ * @brief  Check if the set digits do not conflict with one another.
  *
- * @note The squares that do not have set digits are not checked!
- * @param sudoku 2D array of digit bitsets
+ * @note  The squares that do not have set digits are not checked!
+ *
+ * @param sudoku  2D array of digit bitsets
  */
 bool is_valid(unsigned int sudoku[9][9]);
 
 /**
- * @brief Solve the sudoku using elimination as much as possible
- * without guessing or backtracking.
+ * @brief  Solve the sudoku using elimination as much as possible
+ *         without guessing or backtracking.
  *
- * If you fully solve the sudoku return true, otherwise false.
+ * If the sudoku is fully solved, return true, otherwise false.
  *
- * @note In case the sudoku is invalid, reports to user with one
- * line message on STDERR and returns false. The state of sudoku
- * is then not defined.
- *
- * @param sudoku 2D array of digit bitsets
+ * @param sudoku  2D array of digit bitsets
  */
 bool solve(unsigned int sudoku[9][9]);
 
-#endif //SUDOKU_H
+#endif  // SUDOKU_H
--- src/sudoku.c
+++ src/sudoku.c
@@ -3,24 +3,24 @@
 #include "../include/sudoku.h"
 #include "../include/auxiliary.h"
 
-bool eliminate_row(unsigned int sudoku[9][9], int row_index)
+bool eliminate_row(unsigned int sudoku[9][9], int row)
 {
-    assert(row_index >= 0 && row_index < 9);
+    assert(row >= 0 && row < 9);
 
     bool eliminated = false;
-    unsigned int done_numbers = row_bitset(sudoku, row_index);
+    unsigned int unique_numbers = row_bitset(sudoku, row);
 
     for (int i = 0; i < 9; i++) {
 
-        unsigned int bitset = sudoku[row_index][i];
+        unsigned int bitset = sudoku[row][i];
         if (bitset_is_unique(bitset))
             continue;
 
         int number = 0;
         while ((number = bitset_next(bitset, number)) != -1) {
-            if (bitset_is_set(done_numbers, number)) {
+            if (bitset_is_set(unique_numbers, number)) {
 
-                sudoku[row_index][i] = bitset_drop(bitset, number);
+                sudoku[row][i] = bitset_drop(sudoku[row][i], number);
                 eliminated = true;
             }
         }
@@ -29,24 +29,24 @@ bool eliminate_row(unsigned int sudoku[9][9], int row_index)
     return eliminated;
 }
 
-bool eliminate_col(unsigned int sudoku[9][9], int col_index)
+bool eliminate_col(unsigned int sudoku[9][9], int col)
 {
-    assert(col_index >= 0 && col_index < 9);
+    assert(col >= 0 && col < 9);
 
     bool eliminated = false;
-    unsigned int done_numbers = col_bitset(sudoku, col_index);
+    unsigned int unique_numbers = col_bitset(sudoku, col);
 
     for (int i = 0; i < 9; i++) {
 
-        unsigned int bitset = sudoku[i][col_index];
+        unsigned int bitset = sudoku[i][col];
         if (bitset_is_unique(bitset))
             continue;
 
         int number = 0;
         while ((number = bitset_next(bitset, number)) != -1) {
-            if (bitset_is_set(done_numbers, number)) {
+            if (bitset_is_set(unique_numbers, number)) {
 
-                sudoku[i][col_index] = bitset_drop(bitset, number);
+                sudoku[i][col] = bitset_drop(sudoku[i][col], number);
                 eliminated = true;
             }
         }
@@ -55,23 +55,23 @@ bool eliminate_col(unsigned int sudoku[9][9], int col_index)
     return eliminated;
 }
 
-bool eliminate_box(unsigned int sudoku[9][9], int row_index, int col_index)
+bool eliminate_box(unsigned int sudoku[9][9], int row, int col)
 {
-    assert(row_index >= 0 && row_index < 9);
-    assert(col_index >= 0 && col_index < 9);
+    assert(row >= 0 && row < 9);
+    assert(col >= 0 && col < 9);
 
     bool eliminated = false;
-    unsigned int done_numbers = box_bitset(sudoku, row_index, col_index);
+    unsigned int unique_numbers = box_bitset(sudoku, row, col);
 
-    for (int i = row_index; i < row_index + 3; i++) {
-        for (int j = col_index; j < col_index + 3; j++) {
+    for (int i = row; i < row + 3; i++) {
+        for (int j = col; j < col + 3; j++) {
 
             if (bitset_is_unique(sudoku[i][j]))
                 continue;
 
             int number = 0;
             while ((number = bitset_next(sudoku[i][j], number)) != -1) {
-                if (bitset_is_set(done_numbers, number)) {
+                if (bitset_is_set(unique_numbers, number)) {
 
                     sudoku[i][j] = bitset_drop(sudoku[i][j], number);
                     eliminated = true;
@@ -106,8 +106,7 @@ bool is_valid(unsigned int sudoku[9][9])
         for (int j = 0; j < 9; j++) {
 
             if (sudoku[i][j] == 0x0 ||
-                (j % 3 == 0 && i % 3 == 0 &&
-                 box_conflict(sudoku, i, j)))
+                (i % 3 == 0 && j % 3 == 0 && box_conflict(sudoku, i, j)))
                 return false;
         }
     }
